69ae6beb6c3dcaa1deab729f97c3db3d60febf30
myrechen <1972375779@qq.com>
2021-09-27 20:49:12 +0800

    添加路由，md5加密

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+var router = require('./router.js')
 
 var app = express()
 
-//开放静态资源
+// 开放静态资源
 app.use('/public/', express.static(path.join(__dirname,'./public/')))
 app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
 
-//配置art-template
+// 配置art-template
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
 
-app.get('/', function(req,res){
-	res.render('index.html',{ name:"世界"})
-})
+// 配置body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(router)
 
 
 app.listen(5000,function(){
